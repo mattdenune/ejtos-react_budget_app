f7da376db3b75a974ae4131e532fba66ed9b59cb
mattdenune <mattdenune@gmail.com>
2024-06-25 08:41:32 -0400

    finished practive app

--- src/App.js
+++ src/App.js
@@ -6,38 +6,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
-
+import Remaining from './components/Remaining';
 import { AppProvider } from './context/AppContext';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
+
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                <div className='row mt-3'>
+                    <Budget />
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
+                    <Remaining />
 
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
+                    <ExpenseTotal />
 
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
+                    <ExpenseList />
 
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
+                    <ExpenseItem />
+
+                    <AllocationForm />
 
                 </div>
             </div>
